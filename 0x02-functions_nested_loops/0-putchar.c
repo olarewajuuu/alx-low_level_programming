fbc02263962242bbf89e4c793e3057c2b00f7336
Olarewaju Damilare <damilareolarewaju@yahoo.com>
2023-02-22 03:48:49 -0800

    task done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
 #include "main.h"
-
 /**
- * main - Entr point
- *
- * Description: A program that prints _putchar followed by a new line
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+* Return: 0 success
+*/
+
 int main(void)
 {
-	char text[8] = "_putchar";
-	int i = 0;
+	char str[] = "_putchar";
+	int i;
 
 	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+		_putchar(str[i]);
 	_putchar('\n');
 	return (0);
 }
